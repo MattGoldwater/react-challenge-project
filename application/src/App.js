2f1f0f78fbebc40fc405eda0c00368f4d97f1f9a
Matthew Goldwater <mattgwater@gmail.com>
2020-04-08 12:17:49 -0700

    chore(App) configure redux persist

--- application/src/App.js
+++ application/src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
 import AppRouter from './router/appRouter';
-import store from './redux/store';
+import {store, persistor} from './redux/store';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
         <AppRouter />
+      </PersistGate>
       </Provider>
     )
   }
--- application/src/redux/store.js
+++ application/src/redux/store.js
@@ -1,7 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' 
 import reducers from './reducers';
 
-const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+const persistConfig = {
+  key: 'root',
+  storage,
+}
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, reducers)
+
+export const store = createStore(persistedReducer, {}, applyMiddleware(ReduxThunk));
+export const persistor = persistStore(store);
+
